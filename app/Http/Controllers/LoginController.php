0b684adca39c2e59d5891e95d9ab5b80aa027f2a
Bruno Lopes Silva <brunolopessilva34@gmail.com>
2022-09-15 16:48:50 -0300

    added login system

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,82 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
+        if (Auth::check()) {
+            return redirect('/');
+        }
         return view('login');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function authenticate(Request $request)
     {
-        //
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ],[
+            'email.required' => 'Preencha o campo de email',
+            'email.email' => 'Email inválido',
+            'password.required' => 'Preencha o campo de senha',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+
+        return redirect()->route('user.login')->with('error', 'Credenciais incorretas!');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function destroy(Request $request)
     {
-        //
-    }
+        Auth::logout();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return redirect('/');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,10 +17,12 @@
                 <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Acesse sua conta
                 </h1>
-                <form class="space-y-4 md:space-y-6" action="#" method="POST">
+                <form class="space-y-4 md:space-y-6" action="{{ route('user.authenticate') }}" method="POST">
+                    @csrf
+
                     <div>
-                        <label for="usuario" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seu usuário ou email</label>
-                        <input type="usuario" name="usuario" id="usuario" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Usuário ou email" required="">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seu usuário ou email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Usuário ou email" required="">
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
@@ -31,20 +33,27 @@
                     </div>
                     <button type="submit" class="w-full bg-blue-600 text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Entrar</button>
                     <p class="text-sm text-center font-light text-gray-500 dark:text-gray-400">
-                        Não tem uma conta? <a href="criar-conta.html" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Criar conta</a>
+                        Não tem uma conta? <a href="{{ route('user.register')  }}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Criar conta</a>
                     </p>
                 </form>
             </div>
         </div>
-        <div id="alert" class="mt-4 sm:w-1/4">
-            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center" role="alert">
-                <strong class="font-bold">Erro ao logar!</strong>
-                <span class="block sm:inline">Credenciais incorretas!</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg class="fill-current h-6 w-6 text-red-500" onclick="closeAlert()" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        @if ($errors->any() || session('error'))
+            <div id="alert" class="mt-4 sm:w-1/4">
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center" role="alert">
+                    @if(session('error'))
+                        <span class="block sm:inline">{{ session('error') }}</span>
+                    @else
+                        @foreach ($errors->all() as $error)
+                            <span class="block sm:inline">{{ $error }}</span><br>
+                        @endforeach
+                    @endif
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg class="fill-current h-6 w-6 text-red-500" onclick="closeAlert()" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
                     </span>
+                </div>
             </div>
-        </div>
+        @endif
     </div>
 </section>
 <script type="text/javascript">
